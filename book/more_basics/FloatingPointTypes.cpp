6b6ad4f7bfbdf1f57558ffc6da9e439087fd1e44
DylanFSmith <dsmith@inky>
2019-03-20 22:09:45 -0500

    FloatingPointType chapter.

--- book/more_basics/FloatingPointTypes.cpp
+++ book/more_basics/FloatingPointTypes.cpp
@@ -8,6 +8,28 @@
 #include <iostream>
 #include <type_traits>
 
+//*********************************************************
+// comparison
+//*********************************************************
+
+bool areClose(double x, double y, double tolerance) {
+	return std::abs(x - y) < tolerance;
+}
+
+bool areRelativelyClose(
+	double x,
+	double y,
+	double relativeTolerance = 1e-6
+) {
+	double absoluteTolerance =
+		std::max(
+			std::abs(x)*relativeTolerance,
+			std::abs(y)*relativeTolerance
+		);
+	return areClose(x, y, absoluteTolerance);
+}
+
+
 int main(int argc, char ** argv) {
 	luanics::utility::Herald herald("FloatingPointTypes");
 
@@ -22,20 +44,26 @@ int main(int argc, char ** argv) {
 	// Binary floating point
 	//*********************************************************
 
-	bool constexpr isIeee754 = std::numeric_limits<float>::is_iec559;
-	std::cout << "is IEEE-754 supported = " << isIeee754 << std::endl;
+	{
+		bool constexpr isIeee754 = std::numeric_limits<float>::is_iec559;
+		std::cout << "is IEEE-754 supported = " << isIeee754 << std::endl;
+	}
 
 	//*********************************************************
-	// Floating point categories
+	// Special values
 	//*********************************************************
 
 	if constexpr (std::numeric_limits<double>::has_quiet_NaN) {
+		std::cout << "quiet nan supported" << '\n';
 		constexpr double nan = std::numeric_limits<double>::quiet_NaN();
 		double values[] = {1.0, 2.5, nan, 5.0, nan};
 
 		double const f = nan;
 		double const c = (f - 32.0) * 5.0/9.0;
 		assert(std::isnan(c));
+	}
+	else {
+		std::cout << "quiet nan not supported" << '\n';
 	}
 
 	if constexpr (std::numeric_limits<double>::has_signaling_NaN) {
@@ -54,7 +82,7 @@ int main(int argc, char ** argv) {
 	}
 
 	//*********************************************************
-	// Floating-point literals
+	// Literals
 	//*********************************************************
 
 	{
@@ -68,10 +96,14 @@ int main(int argc, char ** argv) {
 		auto const h = 5l;     // OK - long int literal
 		auto const i = 6.f;    // OK - float literal
 //		auto const j = 7f;     // ERROR - float suffix with integer literal
+
+		static_assert(0x1.0p1 == 2.0);
+		static_assert(0x1.0p0 == 1.0);
+		static_assert(0x1.0p-1 == 0.5);
 	}
 
 	//*********************************************************
-	// Floating-point IO
+	// IO
 	//*********************************************************
 
 	{
@@ -82,8 +114,10 @@ int main(int argc, char ** argv) {
 	}
 
 	{
-		std::cout << "0.1 (fixed-2)      = " << std::setprecision(2) << std::fixed << 0.1 << '\n';
-		std::cout << "0.1 (scientific-2) = " << std::setprecision(2) << std::scientific << 0.1 << '\n';
+		std::cout << "0.1 (fixed-2)      = "
+			<< std::setprecision(2) << std::fixed << 0.1 << '\n';
+		std::cout << "0.1 (scientific-2) = "
+			<< std::setprecision(2) << std::scientific << 0.1 << '\n';
 	}
 
 	//*********************************************************
@@ -113,16 +147,39 @@ int main(int argc, char ** argv) {
 	// overflow and divide by zero
 	//*********************************************************
 
-//	using FloatTraits = std::numeric_limits<float>;
-//	bool const isIeee754Supported = FloatTraits::is_iec559;
-//	if (isIeee754Supported) {
-//		assert(FloatTraits::has_infinity);
-//		assert(FloatTraits::has_quiet_NaN);
-//		assert(FloatTraits::has_signaling_NaN);
-//
-//		assert(1.0f/0.0f == FloatTraits::infinity());
-//		assert(0.0f/0.0f == FloatTraits::signaling_NaN());
-//	}
+	if constexpr (std::numeric_limits<double>::is_iec559) {
+		double const max = std::numeric_limits<double>::max();
+		std::cout << " 1.0/0.0 = " << 1.0/0.0 << '\n';
+		std::cout << "-1.0/0.0 = " << -1.0/0.0 << '\n';
+		std::cout << " 2.0*max = " << 2.0*max << '\n';
+		std::cout << "-2.0*max = " << -2.0*max << '\n';
+	}
+
+	//*********************************************************
+	// comparison
+	//*********************************************************
+
+	assert(areRelativelyClose(0.1 + 0.2, 0.3));
+	assert(not areRelativelyClose(0.0, 0.0));
+
+	//*********************************************************
+	// Library functions
+	//*********************************************************
+
+	assert(std::isfinite(1.0));
+	assert(std::isinf(1.0/0.0));
+	assert(std::isnan(0.0/0.0));;
+
+	assert(std::ilogb(0x1.0p-1) == -1);
+	assert(std::logb(0x1.0p-1) == -1.0);
+	double i;
+	assert(std::modf(0x1.0p-1, &i) == 0.5);
+	assert(std::signbit(0x1.0p-1) == false);
+
+	assert(std::ceil(2.1) == 3.0);
+	assert(std::floor(2.9) == 2.0);
+	assert(std::round(2.5) == 3.0);
+	assert(std::trunc(2.5) == 2.0);
 
 	return 0;
 }
